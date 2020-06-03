c4fb40015d904f87e5b6d3377f5ba90f859110a0
William R Sweeney <wrsweeney2@gmail.com>
2020-06-02 23:59:34 -0400

    add batch dataset construction

---
+++ build_dataset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate insight
+
+python ./src/build_data_set/extract_timeline.py
+python ./src/build_data_set/extract_relevant_chart_events.py
