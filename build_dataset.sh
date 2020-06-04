7ed9b4ceea56ba5588837add61d2151aa676744b
William R Sweeney <wrsweeney2@gmail.com>
2020-06-04 02:07:54 -0400

    split file into two

--- build_dataset.sh
+++ build_dataset.sh
@@ -3,6 +3,7 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate insight
 
+python ./src/build_data_set/chart_flagged_by_vent.py
 python ./src/build_data_set/extract_timeline.py
 python ./src/build_data_set/extract_relevant_chart_events.py
 python ./src/build_data_set/extract_rest.py
